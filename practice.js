fb6bcec7224a23fb034726d733756a1c4b5f61dd
Randy Kim <rhkim1292@gmail.com>
2022-11-13 17:29:13 -0800

    Implement the caesarCipher function and tests

--- practice.js
+++ practice.js
@@ -28,4 +28,26 @@ const calculator = {
     },
 }
 
-export {capitalize, reverseString, calculator}
+function toCharCode(char) {
+    return char.charCodeAt(0);
+}
+
+function shiftCharCode(charCode) {
+    if (!(charCode > 64 && charCode < 91) && !(charCode > 96 && charCode < 123)) return charCode;
+    if (charCode === 90 || charCode === 122) return charCode - 25;
+    return charCode + 1;
+}
+
+function convertToChar(charCode) {
+    return String.fromCharCode(charCode);
+}
+
+function caesarCipher(str) {
+    const strArr = str.split('');
+    const strNumArr = strArr.map(toCharCode);
+    const shiftedArr = strNumArr.map(shiftCharCode);
+    const shiftedStr = shiftedArr.map(convertToChar).join('');
+    return shiftedStr;
+}
+
+export {capitalize, reverseString, calculator, caesarCipher}
--- practice.test.js
+++ practice.test.js
@@ -1,25 +1,52 @@
-import {capitalize, reverseString, calculator} from './practice.js'
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+} from './practice.js';
 
 test('checks if a string has its first character capitalized', () => {
-    expect(capitalize('string')).toBe('String');
+  expect(capitalize('string')).toBe('String');
 });
 
 test('reverses a given string', () => {
-    expect(reverseString('string')).toBe('gnirts');
+  expect(reverseString('string')).toBe('gnirts');
 });
 
-test('adds 1 + 2 to equal 3', () => {
+describe('the calculator object', () => {
+  test('adds 1 + 2 to equal 3', () => {
     expect(calculator.add(1, 2)).toBe(3);
-});
+  });
 
-test('subtracts 1 - 2 to equal -1', () => {
+  test('subtracts 1 - 2 to equal -1', () => {
     expect(calculator.subtract(1, 2)).toBe(-1);
-});
+  });
 
-test('divides 6 / 3 to equal 2', () => {
+  test('divides 6 / 3 to equal 2', () => {
     expect(calculator.divide(6, 3)).toBe(2);
+  });
+
+  test('multiplies 8 * 3 to equal 24', () => {
+    expect(calculator.multiply(8, 3)).toBe(24);
+  });
 });
 
-test('multiplies 8 * 3 to equal 24', () => {
-    expect(calculator.multiply(8, 3)).toBe(24);
+describe('caesarCipher function', () => {
+  test('caesar cipher of "caesar" should equal "dbftbs"', () => {
+    expect(caesarCipher('caesar')).toBe('dbftbs');
+  });
+
+  test('strings with z wrap correctly back to a', () => {
+    expect(caesarCipher('zebra')).toBe('afcsb');
+  });
+
+  test('keep the same case across entire string', () => {
+    expect(caesarCipher('HeLlO WoRlD')).toBe('IfMmP XpSmE');
+  });
+
+  test("punctuation doesn't get shifted", () => {
+    expect(caesarCipher("Hello world, isn't today great?")).toBe(
+      "Ifmmp xpsme, jto'u upebz hsfbu?"
+    );
+  });
 });
